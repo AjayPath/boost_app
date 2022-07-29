505f42b3baa48f19523ed34ef3acbdd710b664bc
AjayPath <33578318+AjayPath@users.noreply.github.com>
2022-07-29 11:45:08 -0400

    Update setup_db.py

--- setup_db.py
+++ setup_db.py
@@ -20,39 +20,45 @@ c = connection.cursor()
 
 
 # Table 1 - app_users table
-c.execute('''CREATE TABLE IF NOT EXISTS app_users
-               (user_id TEXT PRIMARY KEY,
-                team_id TEXT,
-                current_DRECs INTEGER,
-                gifted_DRECs INTEGER)''')
+# c.execute('''CREATE TABLE IF NOT EXISTS app_users
+#                (user_id TEXT PRIMARY KEY,
+#                 team_id TEXT,
+#                 current_DRECs INTEGER,
+#                 gifted_DRECs INTEGER)''')
 
 # Table 2 - team_sites table
-c.execute('''CREATE TABLE IF NOT EXISTS team_sites
-               (team_id TEXT,
-                site_name TEXT,
-                remaining_DRECs INTEGER)''')
+# c.execute('''CREATE TABLE IF NOT EXISTS team_sites
+#                (team_id TEXT,
+#                 site_name TEXT,
+#                 remaining_DRECs INTEGER)''')
 
 # Table 3 - data_log table
-c.execute('''CREATE TABLE IF NOT EXISTS data_log
-               (user_id TEXT,
-                team_id TEXT,
-                site_name TEXT,
-                redeemed_DRECs INTEGER)''')
+# c.execute('''CREATE TABLE IF NOT EXISTS data_log
+#                (user_id TEXT,
+#                 team_id TEXT,
+#                 site_name TEXT,
+#                 redeemed_DRECs INTEGER)''')
 
 
 # Insert rows of data into each of the table in order to test from.
+# Comment out if deleting values
 
 # Insert users into the database by typing your user_id into the first '' in the brackets. Then enter your team_id into the second ''.
 # c.execute("INSERT INTO app_users VALUES ('U03L471LSD6', 'T03KT43EB9P', 10, 7)")
 
 # # Next we will populate the team_sites table using example information for the sake of the demo. When using the real powertrust database, this information will be pulled from there.
-c.execute("INSERT INTO team_sites VALUES ('TQX94CDQT', 'Wenchi', 50)")
-c.execute("INSERT INTO team_sites VALUES ('TQX94CDQT', 'Steung_Chrov', 50)")
-c.execute("INSERT INTO team_sites VALUES ('TQX94CDQT', 'Lingshed_Monastary', 50)")
+# c.execute("INSERT INTO team_sites VALUES ('TQX94CDQT', 'Wenchi', 50)")
+# c.execute("INSERT INTO team_sites VALUES ('TQX94CDQT', 'Steung_Chrov', 50)")
+# c.execute("INSERT INTO team_sites VALUES ('TQX94CDQT', 'Lingshed_Monastary', 50)")
 
 # The information for the data_log table should be a combination of information pulled from the powertrust database as well as information recorded overtime.
 # This is the main information that will be shown on each user profile. For the demo we have entered in some dummy data for my slack account.
 # c.execute("INSERT INTO data_log VALUES ('U03L471LSD6', 'T03KT43EB9P', 'SITE_A', 2)")
+
+# Deleting Values
+# Comment out when adding values into table
+# c.execute("DELETE FROM app_users WHERE user_id = U03L471LSD6")
+# c.execute("DELETE * FROM team_sites ")
 
 # To save these changes use the commit command
 connection.commit()
